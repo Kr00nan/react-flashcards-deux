356d0ee9edfe06748d6a1b4bbcee0ed88a5829b3
Michael Kroon <kroonology@gmail.com>
2019-09-12 16:28:03 -0600

    build out navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import NoMatch from './components/NoMatch';
 
 const App = () => (
   <Fragment>
-    <Switch>
-      <Route exact path='/' component={ Home } />
-      <Route exact path='/about' component={ About } />
-      <Route component={ NoMatch } />
-    </Switch>
+    <NavBar />
+    <Container>
+      <Switch>
+        <Route exact path='/' component={ Home } />
+        <Route exact path='/about' component={ About } />
+        <Route component={ NoMatch } />
+      </Switch>
+    </Container>
   </Fragment>
 );
 
